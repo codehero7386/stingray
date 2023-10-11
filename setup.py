dc0ae6ec38f68c34cc44a6679cccc5ba0787f7d3
codehero7386 <56253286+codehero7386@users.noreply.github.com>
2023-10-11 22:34:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,31 +6,19 @@
 
 import os
 import sys
-
 from setuptools import setup
 
-# First provide helpful messages if contributors try and run legacy commands
-# for tests or docs.
-
+# Provide helpful messages for contributors running tests or building documentation.
 TEST_HELP = """
-Note: running tests is no longer done using 'python setup.py test'. Instead
-you will need to run:
+Note: Running tests and building documentation has changed. Use the following commands:
 
+To run tests:
     tox -e test
 
-If you don't already have tox installed, you can install it with:
+To build documentation:
+    tox -e build_docs
 
-    pip install tox
-
-If you only want to run part of the test suite, you can also use pytest
-directly with::
-
-    pip install -e .[test]
-    pytest
-
-For more information, see:
-
-  http://docs.astropy.org/en/latest/development/testguide.html#running-tests
+For more information, see the development guide.
 """
 
 if 'test' in sys.argv:
@@ -38,34 +26,20 @@ if 'test' in sys.argv:
     sys.exit(1)
 
 DOCS_HELP = """
-Note: building the documentation is no longer done using
-'python setup.py build_docs'. Instead you will need to run:
+Note: Building documentation has changed. Use the following commands:
 
+To build documentation:
     tox -e build_docs
 
-If you don't already have tox installed, you can install it with:
-
-    pip install tox
-
-You can also build the documentation with Sphinx directly using::
-
-    pip install -e .[docs]
-    cd docs
-    make html
-
-For more information, see:
-
-  http://docs.astropy.org/en/latest/install.html#builddocs
+For more information, see the installation guide.
 """
 
 if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
     print(DOCS_HELP)
     sys.exit(1)
 
+# Define a template for version retrieval.
 VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
 try:
     from setuptools_scm import get_version
     version = get_version(root='..', relative_to=__file__)
@@ -73,8 +47,33 @@ except Exception:
     version = '{version}'
 """.lstrip()
 
+# Use setuptools_scm to retrieve the version and write it to a version.py file.
 setup(
     use_scm_version={
         'write_to': os.path.join('stingray', 'version.py'),
         'write_to_template': VERSION_TEMPLATE
-    })
+    },
+    name='your-package-name',  # Replace with your package name
+    description='A short description of your package',
+    long_description='A longer description of your package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/your-package-name',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        # List your package's dependencies here
+        'numpy',
+        'scipy',
+    ],
+    extras_require={
+        'docs': ['sphinx'],
+        'test': ['pytest'],
+    },
+    packages=['stingray'],  # Replace with your package name
+    include_package_data=True,
+)
